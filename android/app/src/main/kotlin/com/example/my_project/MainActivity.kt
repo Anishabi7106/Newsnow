44bc9fbaf5d1f118563e6212a234913863a92fcb
Anishabi7106 <abishabi862@gmail.com>
2024-11-15 11:37:41 +0530

    delete

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.mycompany.newsnow
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/index.dart
+++
@@ -1,3 +0,0 @@
-// Export pages
-export '/loginpage/loginpage_widget.dart' show LoginpageWidget;
-export '/home/home_widget.dart' show HomeWidget;
